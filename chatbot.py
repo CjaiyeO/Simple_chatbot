4dbbe5f2fdb77cf7348f0435309e6d3c85e3d17d
Ajay Sankhari <156953240+CjaiyeO@users.noreply.github.com>
2024-02-09 12:49:03 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,5 +3,13 @@ import openai
 #before runing enter your api key
 openai.api_key = "sk-"
 
-answer = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "What is neural network"}])
-print(answer.choices[0].message.content)
+question = input("How can I help you today?\n")
+
+response = openai.Completion.create(
+    engine="gpt-3.5-turbo",
+    prompt=question,
+    temperature=0.7,       # Decides the randomness ranges from 0-1 (low = logical response; high = creative response)
+    max_tokens=150         # Max number of words in your response
+)
+print(response.choices[0].text.strip())
+
