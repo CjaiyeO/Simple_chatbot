59d9f944d12e888a0a52127ff63d3e658c993512
Ajay Sankhari <156953240+CjaiyeO@users.noreply.github.com>
2024-02-09 11:58:23 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,5 +3,5 @@ import openai
 #before runing enter your api key
 openai.api_key = "sk-"
 
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "What is neural network"}])
-print(completion.choices[0].message.content)
+answer = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "What is neural network"}])
+print(answer.choices[0].message.content)
